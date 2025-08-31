f7ddc6baa759d78fe92eb934752ba4863d9dff58
Sergei Lebedev <lebedev.se@algorithmics.ru>
2025-08-31 20:04:32 +0300

    ДЗ к занятию 8 Асинхронные операции

--- scala213Module/src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
+++ scala213Module/src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
@@ -3,6 +3,7 @@ package ru.otus.module1.futures
 import ru.otus.module1.futures.HomeworksUtils.TaskSyntax
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
@@ -15,12 +16,26 @@ object task_futures_sequence {
    * в правовой результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ec: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    val empty: Future[(List[A], List[Throwable])] =
+      Future.successful(Nil, Nil)
 
+    futures.foldLeft(empty) { (accumulator, future) =>
+      accumulator.flatMap {
+        case (successes, failures) =>
+          future.transform {
+            case Success(a) => Success((a +: successes, failures))
+            case Failure(e) => Success(successes, e :: failures)
+          }
+      }
+    }.map {
+      case (successes, failures) => (successes.reverse, failures.reverse)
+    }
+  }
 }
